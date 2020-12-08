0ddb945ecb8ff6ebe745adb46dd7765f69b4cf84
ZhangLei012 <1309427117@qq.com>
2020-12-08 11:49:40 +0800

    删除密钥

--- config/oss.go
+++ config/oss.go
@@ -7,7 +7,7 @@ const (
 	//OSSEndpoint oss endpoint
 	OSSEndpoint = "oss-cn-beijing.aliyuncs.com"
 	//OSSAccessKeyID 访问id 在aliyun网站上创建ram用户获取
-	OSSAccessKeyID = "LTAI4FzFyFfDeabhps5bZBMo"
+	OSSAccessKeyID = "xxxx"
 	//OSSAccessKeySecret 访问密钥
-	OSSAccessKeySecret = "MnE1y7KsZlbeNo2rCIIdNOvkYltNpf"
+	OSSAccessKeySecret = "yyyy"
 )
